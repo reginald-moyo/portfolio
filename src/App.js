2c69ee02f1463f176d0808128fa4b97c3650d83e
Reginald Moyo <regimoyo@gmail.com>
2026-02-05 20:08:41 +0100

    removed browser router and added hashrouter

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Home from "./routes/Home";
 import Project from "./routes/Project";
 import About from "./routes/About";
 import Contact from "./routes/Contact";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <>
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    
+    
   );
 }
 
