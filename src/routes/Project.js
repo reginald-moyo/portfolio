dd61f5af3154998f7f63a5cee7d64cb60938a102
Reginald Moyo <regimoyo@gmail.com>
2026-02-24 01:41:28 +0100

    changed the favicon, adjusted font side in about section to be smaller on smaller screen and bigger on bigger screen

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -10,7 +10,7 @@ const Project = () => {
   return (
   <div>
     <Navbar />
-    <HeroImg2 heading="PROJECTS" text="The projects below showcase my journey as a developer. I’ve used them to experiment with new technologies, solve practical problems, and continuously improve my technical skills. Each one reflects my commitment to learning, adaptability, and building web experiences that are both efficient and accessible. My background in client service also influences how I approach these projects, prioritizing clarity, usability, and solutions that genuinely serve user needs." />
+    <HeroImg2 heading="Summary" text="The projects below showcase my journey as a developer. I’ve used them to experiment with new technologies, solve practical problems, and continuously improve my technical skills. Each one reflects my commitment to learning, adaptability, and building web experiences that are both efficient and accessible. My background in client service also influences how I approach these projects, prioritizing clarity, usability, and solutions that genuinely serve user needs." />
     <Work />
     <Footer />
   </div>
